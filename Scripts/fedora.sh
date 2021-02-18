702b07dfff30db10c45605227c917e7cef2608fa
Tran Hau <hauvipapro@gmail.com>
2021-02-18 16:02:55 +0700

    add vim-plug

--- Scripts/fedora.sh
+++ Scripts/fedora.sh
@@ -18,3 +18,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 # https://github.com/junegunn/fzf#using-git
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+# https://github.com/junegunn/vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
