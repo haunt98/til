6af669b2b9c1bbd0700fdaec436d171766a83df7
Tran Hau <hauvipapro@gmail.com>
2021-02-18 16:09:45 +0700

    add golangci-lint for scripts

--- Scripts/fedora.sh
+++ Scripts/fedora.sh
@@ -23,3 +23,6 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 # https://github.com/junegunn/vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# https://golangci-lint.run/usage/install/#local-installation
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.37.0
